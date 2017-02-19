a64632b79dbfa0fcdcdfb116664298a26f6bb361
Michael Miller <michaelalanmiller@gmail.com>
2017-02-18 20:39:06 -0500

    Updated non-English extract features

--- extract_cs_features.py
+++ extract_cs_features.py
@@ -1,35 +1,21 @@
 import pandas as pd
 import csv, pdb
-from string import ascii_letters
-from nltk.corpus import words, stopwords
-import nltk
+from string import ascii_letters, punctuation
+letters = ascii_letters + punctuation
 from collections import Counter, defaultdict
 import re, hashlib, os, sys
-endict = set(words.words())
-from pandas.tseries.offsets import *
-#from sklearn.metrics import cohen_kappa_score, make_scorer
-#from sklearn import cross_validation, svm
-#from sklearn.linear_model import LinearRegression, LogisticRegression
-#from sklearn.feature_extraction.text import CountVectorizer
-#from sklearn.model_selection import train_test_split, LeaveOneOut
-#from scipy.stats import ttest_rel, ttest_ind, pearsonr
-#from scipy.sparse import hstack
+#from pandas.tseries.offsets import *
 import numpy as np
 import math
 
-
 """ I/O definitions """
-#featfile = '/home/michael/school/research/wp/ar/discussion_features.csv'
-featfile = sys.argv[1]
+featfile = sys.argv[1] # input file
 feats = pd.read_csv(featfile)
 
-
-
-
-# ## technical terms in En
-# Doesn't have to have code-switching
+outfile = sys.argv[2]
 
 """ utility fns """
+# TODO: Make more meaningful
 def en_technical(text):
 #     if not has_english(text): return 0.0
     en_wds = [w for w in str(text).split() if len(w) > 1 and w.lower() in endict and w != 'TEMPLATE']
@@ -43,13 +29,13 @@ def en_technical(text):
 def two_quotes(text):
     return str(text).count('"') >= 2
 
-# ## proportion of switches
+# ## proportion of switches between Latin and non-Latin words
 # Doesn't have to have code-switching
 def prop_switches(text):
-    en = [w in endict for w in str(text).split() if len(w) > 1]
-    n_switches = sum(en[i] != en[i+1] for i in range(len(en)-1))
-    if len(en) > 1:
-        return n_switches/(len(en)-1)
+    wds = [all_latin(w) for w in str(text).split()]
+    n_switches = sum(wds[i] != wds[i+1] for i in range(len(wds)-1))
+    if len(wds) > 1:
+        return n_switches/(len(wds)-1)
     else:
         return 0.0
 
@@ -65,10 +51,40 @@ def has_english(text):
     else:
         return False
 
+# See if a word is all Latin characters
+def all_latin(word):
+    if len(word) > 1 and all(c in letters for c in word) \
+            and any(c in ascii_letters for c in word) \
+            and not word.startswith('http') \
+            and word != '(UTC)' \
+            and word != '\\n' \
+            and not 'TEMPLATE' in word:
+        return True
+    else:
+        return False
+
+# See if a text has at least three words with Latin letters longer than 1 letter
+def has_latin(text):
+    if sum(all_latin(w) for w in str(text).split()) > 2:
+        return True
+    else:
+        return False
+
+# Proportion of words with all Latin characters
+def prop_latin(text):
+    n_wds = sum(all_latin(w) for w in str(text).split())
+    return n_wds/len(str(text).split())
+
+# Return words with Latin letters longer than 1 letter
+def get_latin(text):
+    latin_wds = [w for w in str(text).split() if all_latin(w)]
+    if len(latin_wds) > 2:
+        return latin_wds
+    else:
+        return None
 
 
-
-""" fns to run """
+""" extraction functions """
 def extract_technical():
     ed_crit = [0.0 if not a and b else a for a,b in zip(feats['editor_talk'].map(en_technical), feats['en_cs'].tolist())]
     other_crit = [0.0 if not a and b else a for a,b in zip(feats['other_talk'].map(en_technical), feats['other_en_cs'].tolist())]
@@ -83,7 +99,8 @@ def extract_quotes():
     feats['editor_two_quotes'] = ed_crit
     feats['other_two_quotes'] = other_crit
 
-def extract_prop_switches():
+def extract_prop_latin_switches():
+    """ Proportion of switches between Latin and non-Latin words """
     ed_crit = feats['editor_talk'].map(prop_switches)
     other_crit = feats['other_talk'].map(prop_switches)
     feats['editor_prop_switches'] = ed_crit
@@ -95,6 +112,7 @@ def extract_prop_english():
     crit = feats['other_talk'].map(prop_english)
     feats['other_prop_en'] = crit
 
+# TODO: Put in lang identification
 def extract_has_english():
     # Posts with code-switching
     crit = feats['editor_talk'].map(has_english)
@@ -104,20 +122,37 @@ def extract_has_english():
     print("Number of posts with English: {0} / {1}".format(sum(crit), len(feats)))
     print("Number of other posts with English: {0} / {1}".format(sum(crit), len(feats)))
 
+def extract_has_latin():
+    # Posts with code-switching
+    crit = feats['editor_talk'].map(has_latin)
+    feats['latin_cs'] = crit
+    crit = feats['other_talk'].map(has_latin)
+    feats['other_latin_cs'] = crit
+    print("Number of posts with Latin: {0} / {1}".format(sum(crit), len(feats)))
+    print("Number of other posts with Latin: {0} / {1}".format(sum(crit), len(feats)))
+
+def extract_prop_latin():
+    crit = feats['editor_talk'].map(prop_latin)
+    feats['editor_prop_latin'] = crit
+    crit = feats['other_talk'].map(prop_latin)
+    feats['other_prop_latin'] = crit
+
 def get_editor_success():
     feats['editor_success'] = feats['editor_score'].map(lambda x: x > 0.5)
 
 
 def main():
 
-    extract_has_english()
-    extract_prop_english()
+    #extract_has_english()
+    #extract_prop_english()
+    extract_has_latin()
+    extract_prop_latin()
     extract_prop_switches()
     extract_quotes()
-    extract_technical()
+    #extract_technical()
     get_editor_success()
-    feats.to_csv(featfile, index=False)
-    print("Wrote features")
+    feats.to_csv(outfile, index=False)
+    print("Wrote features to {0}".format(outfile))
 
 if __name__ == '__main__':
     main()
